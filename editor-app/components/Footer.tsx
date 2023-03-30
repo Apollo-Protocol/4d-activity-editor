16a8e173dee8690b5d74a508f910fef6a39b9738
T E Baker <t.baker@amrc.co.uk>
2023-03-30 15:53:16 +0100

    Update Footer.tsx

--- editor-app/components/Footer.tsx
+++ editor-app/components/Footer.tsx
@@ -2,9 +2,29 @@ function Footer() {
   const style = {};
   return (
     <>
-      <footer id="footer">
-        <span>@AMRC in collaboration with @CIS</span>
+      <div className="container">
+      <footer className="py-3 my-4">
+        <p className="border-bottom pb-3 mb-3">
+        </p>
+
+        <div className="container">
+  <div className="row">
+    <div className="col-sm">
+    <a href="https://www.amrc.co.uk/">
+    <img src="https://www.amrc.co.uk/img/logo-header-2023.svg?1678105026" className="rounded float-left" alt="..."></img>
+    </a>
+    </div>
+    <div className="col-sm">
+    <p className="text-center text-muted"><p className="text-center text-muted">2023 Apollo Program Activity Diagram Editor</p> <p className="text-center text-muted">Created by AMRC in collaboration with CIS</p></p>
+    </div>
+    <div className="col-sm">
+    <img src="https://www.amrc.co.uk/img/logo-header-2023.svg?1678105026" className="rounded float-left" alt="..."></img>
+    </div>
+  </div>
+</div>
+        
       </footer>
+    </div>
     </>
   );
 }

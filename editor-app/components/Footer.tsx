0f9adedd3d0e3df2e9116b6ebb3ca405258c47f3
T E Baker <t.baker@amrc.co.uk>
2023-04-03 14:33:15 +0100

    Added placeholder feedback link

--- editor-app/components/Footer.tsx
+++ editor-app/components/Footer.tsx
@@ -15,6 +15,7 @@ function Footer() {
     <h5>More</h5>
           <ul className="list-unstyled text-small">
             <li><Link className="text-muted" href="https://digitaltwinhub.co.uk/networks/29-the-apollo-protocol">Get in touch</Link></li>
+            <li><Link className="text-muted" href="/">Give feedback</Link></li>
           </ul>
     
     </div>

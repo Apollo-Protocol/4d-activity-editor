2efef7113e480e0ffd3fb586bcb4fa0d33bbb77d
Kallum Burgin <Kallb123@users.noreply.github.com>
2023-04-04 16:56:42 +0100

    Update Next.JS config with deployment settings

--- editor-app/next.config.js
+++ editor-app/next.config.js
@@ -1,10 +1,29 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = ''
+let basePath = ''
+let images = {
+  unoptimized: true,
+}
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+  images = {
+    loader: 'imgix',
+    path: 'apollo-protocol-editor.imgix.net',
+  }
+}
+
 const nextConfig = {
   reactStrictMode: true,
-  //assetPrefix: "./",
-  images: {
-    unoptimized: true,
-  },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: images,
 };
 
 module.exports = nextConfig;
